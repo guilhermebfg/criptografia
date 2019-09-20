e45a3f32ffa39eadfa66cca4c549c1c6936d415b
guilhermebfg <guilhermebfg@hotmail.com>
2019-09-20 17:08:17 -0300

    reduzindo funcoes

--- main.py
+++ main.py
@@ -2,28 +2,20 @@ def criptografar(mensagem, chave=None):
 	mensagem_final = ''
 	if mensagem:
 		lista = []
-		if chave:
-			# Criptografa de acordo com a chave passada
-			mensagem_final = ''
-		else:
-			# Criptografia padrao para quando nao houver chave
-			for x in range(len(mensagem)):
-				lista.append(mensagem[x])
-				lista[x] = ord(lista[x])
-			lista.reverse()
-			for x in range(len(lista)):
-				if lista[x] == 32:
-					pass
-				else:
-					if 96 < lista[x] < 123:
-						lista[x] += (lista[x] + 5) > 122 and \
-							(5 - 26) or 5
-					elif 64 < lista[x] < 91:
-						lista[x] += (lista[x] + 5) > 90 and \
-							(5 - 26) or 5
-			for x in range(len(lista)):
-				mensagem_final += chr(lista[x])
-	return mensagem_final
+		alt = chave and int(chave) or 5
+		for x in range(len(mensagem)):
+			lista.append(ord(mensagem[x]))
+			if lista[x] == 32:
+				pass
+			else:
+				if 96 < lista[x] < 123:
+					lista[x] += (lista[x] + alt) > 122 and \
+						(alt - 26) or alt
+				elif 64 < lista[x] < 91:
+					lista[x] += (lista[x] + alt) > 90 and \
+						(alt - 26) or alt
+			mensagem_final += chr(lista[x])
+	return mensagem_final[::-1]
 
 def descriptografar(mensagem, chave=None):
 	mensagem_final = ''
@@ -34,11 +26,10 @@ def descriptografar(mensagem, chave=None):
 		else:
 			for x in range(len(mensagem)):
 				lista.append(ord(mensagem[x]))
-				lista[x] = chr((lista[x] - 5) > 96 and lista[x] - 5 or \
-					(lista[x] - 5) > 64 and lista[x] - 5 or lista[x] -\
-					[5 + 26])
+				lista[x] = chr((lista[x] - alt) > 96 and lista[x] - alt or \
+					(lista[x] - alt) > 64 and lista[x] - alt or lista[x] -\
+					[alt + 26])
 			lista.reverse()
 			for x in range(len(lista)):
 				mensagem_final += lista[x]
 	return mensagem_final
-
