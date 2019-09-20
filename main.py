b63190d3f6994720124cbabb2c51b02a4e0bc291
Guilherme Baldoino <guilherme.baldoino@softrh.com.br>
2019-09-20 16:41:31 -0300

    adicionando funcao de descriptografar

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def criptografar(mensagem, chave=None):
-	chave_final = ''
+	mensagem_final = ''
 	if mensagem:
+		lista = []
 		if chave:
 			# Criptografa de acordo com a chave passada
-			chave_final = ''
+			mensagem_final = ''
 		else:
 			# Criptografia padrao para quando nao houver chave
-			lista = []
 			for x in range(len(mensagem)):
 				lista.append(mensagem[x])
 				lista[x] = ord(lista[x])
@@ -22,5 +22,23 @@ def criptografar(mensagem, chave=None):
 						lista[x] += (lista[x] + 5) > 90 and \
 							(5 - 26) or 5
 			for x in range(len(lista)):
-				chave_final += chr(lista[x])
-	return chave_final
+				mensagem_final += chr(lista[x])
+	return mensagem_final
+
+def descriptografar(mensagem, chave=None):
+	mensagem_final = ''
+	if mensagem:
+		lista = []
+		if chave:
+			mensagem_final = ''
+		else:
+			for x in range(len(mensagem)):
+				lista.append(ord(mensagem[x]))
+				lista[x] = chr((lista[x] - 5) > 96 and lista[x] - 5 or \
+					(lista[x] - 5) > 64 and lista[x] - 5 or lista[x] -\
+					[5 + 26])
+			lista.reverse()
+			for x in range(len(lista)):
+				mensagem_final += lista[x]
+	return mensagem_final
+
