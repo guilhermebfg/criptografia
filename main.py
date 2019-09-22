86c3247768008fae833cc281b794df999a88998a
guilhermebfg <guilhermebfg@hotmail.com>
2019-09-22 15:06:55 -0300

    correcao de bugs

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def criptografar(mensagem, chave=None):
             else:
                 if 96 < lista[x] < 123:
                     lista[x] += (lista[x] + alt) > 122 and \
-                        (alt - 26) or alt
+                        (alt - 25) or alt
                 elif 64 < lista[x] < 91:
                     lista[x] += (lista[x] + alt) > 90 and \
-                        (alt - 26) or alt
+                        (alt - 25) or alt
             mensagem_final += chr(lista[x])
     print(mensagem_final[::-1])
 
@@ -30,9 +30,13 @@ def descriptografar(mensagem, chave=None):
         alt = chave and int(chave) or 5
         for x in range(len(mensagem)):
             lista.append(ord(mensagem[x]))
-            lista[x] = (lista[x] - alt) > 96 and lista[x] - alt or \
-                (lista[x] - alt) > 64 and lista[x] - alt or \
-                lista[x] - [alt + 26]
+            print(lista)
+            if 96 < lista[x] < 123:
+                lista[x] -= (lista[x] - alt) > 96 and \
+                    alt or (alt - 25)
+            elif 64 < lista[x] < 91:
+                lista[x] -= (lista[x] - alt) > 64 and \
+                    alt or (alt - 25)
             lista[x] = chr(lista[x])
             mensagem_final += lista[x]
     print(mensagem_final[::-1])
@@ -46,9 +50,9 @@ def tratar_chave(chave):
             pass
         else:
             if 96 < ord(chave[x]) < 123:
-                lista.append(ord(chave[x]) - 97)
+                lista.append(ord(chave[x]) - 96)
             elif 64 < ord(chave[x]) < 91:
-                lista.append(ord(chave[x]) - 65)
+                lista.append(ord(chave[x]) - 64)
     alt = int(sum(lista) / len(lista))
     print(alt)
     return alt
